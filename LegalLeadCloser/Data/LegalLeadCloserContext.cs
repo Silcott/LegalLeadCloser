0f60585ddd83650950afd153919aa41a94591e93
Silcott <silcott.jb@gmail.com>
2021-05-21 08:32:52 -0400

    Created Client Controller

---
+++ LegalLeadCloser/Data/LegalLeadCloserContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using LegalLeadCloser.Models;
+
+namespace LegalLeadCloser.Data
+{
+    public class LegalLeadCloserContext : DbContext
+    {
+        public LegalLeadCloserContext (DbContextOptions<LegalLeadCloserContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<LegalLeadCloser.Models.Clients> Clients { get; set; }
+    }
+}
