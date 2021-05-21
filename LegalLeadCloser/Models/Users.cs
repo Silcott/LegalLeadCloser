2f96cc8e63a3996c5279e36d82ceb5780f725552
Silcott <silcott.jb@gmail.com>
2021-05-20 22:12:50 -0400

    Updated User Model

---
+++ LegalLeadCloser/Models/Users.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LegalLeadCloser.Models
+{
+    public class Users
+    {
+        public int UserID { get; set; }
+        public string CompanyName { get; set; }
+        public string YearsPracticing { get; set; }
+        public string MonthsPracticing { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string FullName { get; set; }
+        public string StreetAddress { get; set; }
+        public string CityAddress { get; set; }
+        public string StateAddress { get; set; }
+        public string ZipCodeAddress { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+        public string ImageName { get; set; }
+        public string CreationDate { get; set; }
+        public string SubscriptionSerivice { get; set; }
+        public string SecurityLevel { get; set; }
+        public string Password { get; set; }
+        public string LastLogin { get; set; }
+
+    }
+}
