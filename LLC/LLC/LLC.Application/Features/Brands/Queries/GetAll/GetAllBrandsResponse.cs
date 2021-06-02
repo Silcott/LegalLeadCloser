55f3ad9096ae87d104e951b9591f2fd3d561a29b
Silcott <silcott.jb@gmail.com>
2021-06-02 11:41:29 -0400

    Update

--- LLC/LLC/LLC.Application/Features/Brands/Queries/GetAll/GetAllBrandsResponse.cs
+++ LLC/LLC/LLC.Application/Features/Brands/Queries/GetAll/GetAllBrandsResponse.cs
@@ -5,9 +5,9 @@ namespace LLC.Application.Features.Brands.Queries.GetAll
     public class GetAllBrandsResponse
     {
         public int Id { get; set; }
-        public string FirstName { get; set; }//Name
-        public string LastName { get; set; }//Description
-        public DateTime CourtDate { get; set; }//Tax
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime CourtDate { get; set; }
         public string CourtLocation { get; set; }
         public string Phone { get; set; }
         public string Email { get; set; }
