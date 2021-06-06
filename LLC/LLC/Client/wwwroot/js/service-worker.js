600e47e50c175d74b0b1fa0b1442d874037df059
Silcott <silcott.jb@gmail.com>
2021-06-06 18:04:33 -0400

    Updated

---
+++ LLC/LLC/Client/wwwroot/js/service-worker.js
@@ -0,0 +1,6 @@
+﻿navigator.serviceWorker.register('/js/service-worker.js', { scope: '/' })
+    .then(function (registration) {
+        console.log('Service worker registered successfully');
+    }).catch(function (e) {
+        console.error('Error during service worker registration:', e);
+    });
